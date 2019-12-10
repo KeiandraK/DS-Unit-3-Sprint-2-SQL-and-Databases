54dc58d13a489a48f1d5af8660706253459ff863
KeiandraK <kking1795@gmail.com>
2019-12-10 00:10:33 -0600

    part 2 of the assignment

---
+++ module1-introduction-to-sql/P2buddymove_holidayiq.py
@@ -0,0 +1,18 @@
+import sqlite3
+import pandas
+
+df= pd.read_csv(buddymove_holidayiq.csv)
+
+def obtain_name(buddymove_holidayiq.sqlite3, df.to_sql):
+    query= df.shape
+    
+    connection = sqlite3.connect(buddymove_holidayiq.sqlite3)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    
+    return results
+    
+    
